99dd0af295886deea7de9d751cdb1b013d8c2270
sourdonny <ismaeldnyjimenez@gmail.com>
2021-10-04 20:04:45 +0200

    Efecto de botón pulsado

--- app/src/main/java/com/example/practica1_dadm/MainActivity.java
+++ app/src/main/java/com/example/practica1_dadm/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView pressedJugar;
+    ImageView pressedSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //pressedJugar = findViewById(R.id.pr);
     }
 
     public void Jugar(View view){
