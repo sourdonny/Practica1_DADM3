4973c5f9652020b22ebbee916b0923bd6e686bf6
sourdonny <ismaeldnyjimenez@gmail.com>
2021-10-08 23:12:24 +0200

    Bocadillo de diálogo para las preguntas.

--- app/src/main/java/com/example/practica1_dadm/MainActivity.java
+++ app/src/main/java/com/example/practica1_dadm/MainActivity.java
@@ -3,29 +3,37 @@ package com.example.practica1_dadm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView pressedJugar;
-    ImageView pressedSalir;
+    MediaPlayer titleMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //pressedJugar = findViewById(R.id.pr);
+        titleMusic = MediaPlayer.create(MainActivity.this, R.raw.blackmamba);
+        titleMusic.start();
     }
+
+    /*@Override
+    protected void OnStop(){
+        super.onStop();
+    }*/
 
     public void Jugar(View view){
         Intent pregunta = new Intent(this, Pregunta.class);
+        titleMusic.stop();
         startActivity(pregunta);
+        finish();
     }
 
     public void Salir(View view){
+        titleMusic.stop();
         finish();
     }
 }
